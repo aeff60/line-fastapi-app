5f3e6a88097d49939545db502a4cc09440cd5733
aeff60 <ssbunklang@gmail.com>
2024-11-20 16:32:10 +0700

    add dotenv

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import requests
+from dotenv import load_dotenv
 
+load_dotenv()
 # สร้าง FastAPI instance
 app = FastAPI()
 
